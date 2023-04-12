3b046a8ebdbe4a550afe836b30eb5e5170b72ea8
bvMGWR <bvMGWR@users.noreply.github.com>
2023-04-12 16:06:25 -0400

    nuevo archivo

---
+++ variables_1.py
@@ -0,0 +1,28 @@
+#01 DECLARANDO VARIABLES
+nombre = "barbara"
+name = "constanza"
+PI = 3,14
+#02 IMPRESION DE VARIABLES
+print (name)
+print ("hola soy", nombre)
+#03 DECLARANDO UNA TERCERA VARIABLES DE TIPO NUMERICO
+edad = 17
+#04 IMPRESION DE TEXTO + VARIABLE
+print ("mi edad es", edad)
+#05 IMPRIMIENDO 2 VARIABLES EN LA MISMA LINEA (simpre separado por comas)
+print ("hola mi nombre es", nombre, "tengo", edad, "años")
+print (f"hola mi nombre es {nombre} y tengo {edad} años")
+#06 ACTUALIZANDO LA VARIABLE (MUTABLE)
+nombre = "barbarita"
+name = "coni"
+#07 imprimirla 
+print ("hola mi nuevo nombre es",nombre)
+name = 1000
+peso = 80.5
+print ("mi nombre es",name, "y peso",peso)
+#08 VARIABLES EN UNA SOLA LINEA (separadas por comas)
+pais, region, cuidad= "chile","los lagos","puerto montt"
+#09 UTILIZANDO LA INSTRUCCION INPUT 
+Nombre1= input("¿cual es tu nombre?")
+edad2= input ("¿cual es tu edad?")
+print("tu nombre es:",Nombre1,"y tu edad es",edad2)
