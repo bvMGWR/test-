c73a1f955f1f88db51669141c5e00e2b4a64d5ac
bvMGWR <bvMGWR@users.noreply.github.com>
2023-06-28 20:30:39 -0400

    nuevo archivo

--- parcial_escrito/ejercicio2.py
+++ parcial_escrito/ejercicio2.py
@@ -12,5 +12,39 @@ E) Crear una función y ordenar la lista de forma alfabética."""
 a = ["Rojo", "Verde", "Celeste", "Violeta"]
 b = ["Osorno", "Puerto Montt", "Puerto Varas", "Valdivia"]
 
+def mas_larga(lista):
+   
+    palabra = ""
+    for i in lista:
+        if len(i) > len(palabra):
+            palabra = i
+    return palabra
 
+def mas_corta(lista):
+   
+    palabra = lista[0]
+    for i in lista:
+        if len(i) < len(palabra):
+            palabra = i
+    return palabra
 
+def concatenar(lista,lista2): 
+   resul= lista + lista2
+   return resul 
+
+def mayusculas(lista):
+   tex= str(lista)
+   may= tex.upper()
+   return may
+
+def ordenar(lista): 
+    lista.sort()
+    return lista
+  
+
+ 
+print("La palabra más larga en la lista 'a' es:", mas_larga(a))
+print("La palabra más corta en la lista 'b' es:", mas_corta(b))
+print(concatenar(a,b))
+print(mayusculas(a))
+print(ordenar(a))
